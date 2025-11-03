835435679b770a0376c4f311824a6dc0c8be8d4e
shaharsol <shahar@gnift.com>
2025-11-03 10:18:21 +0200

    finsied props

--- 10-props/src/components/posts/post/Post.tsx
+++ 10-props/src/components/posts/post/Post.tsx
@@ -6,12 +6,13 @@ interface PostProps {
 }
 export default function Post(props: PostProps) {
 
-    const { title, createdAt, body } = props.post
+    const { title, createdAt, body, user: { name }, comments } = props.post
 
     return (
         <div className='Post'>
             <h3>{title}</h3>
-            <h6>{createdAt}</h6>
+            <h6>by {name} at {(new Date(createdAt)).toLocaleDateString()}</h6>
+            <p>comments: {comments.length}</p>
             <div>{body}</div>
         </div>
     )
